5eef45271ac9b4ec9683bbca55b8e86fbb8c0d8b
yangdongying <91609869@qq.com>
2020-02-06 14:13:29 +0800

    修改介绍图片

--- src/main/java/com/dyman/im/service/impl/UserLoginServiceImpl.java
+++ src/main/java/com/dyman/im/service/impl/UserLoginServiceImpl.java
@@ -1,8 +1,6 @@
 package com.dyman.im.service.impl;
 
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.dyman.im.cache.SocketConnectionCache;
 import com.dyman.im.entity.ChatRoomMessage;
 import com.dyman.im.entity.UserLogin;
 import com.dyman.im.exception.BusinessException;
@@ -11,17 +9,13 @@ import com.dyman.im.mapper.UserLoginMapper;
 import com.dyman.im.service.UserLoginService;
 import com.dyman.im.util.EncryptionUtil;
 import com.dyman.im.util.NicknameGenerator;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
-import sun.security.krb5.internal.PAData;
 
 import javax.servlet.http.HttpServletRequest;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.List;
